4e6e40d1711a9776d8805c88588c1e26c78e4e6c
Taehoon <lth9029@gmail.com>
2022-11-01 20:39:44 +0900

    add code for drawing buoundingbox

--- Detector.py
+++ Detector.py
@@ -33,3 +33,58 @@ class Detector:
 
         get_file(fname=fileName, origin=modelURL, cache_dir=self.cacheDir, cache_subdir="checkpoints", extract=True)
 
+
+    def loadModel(self):
+        print("Loading Model " + self.modelName)
+        tf.keras.backend.clear_session()
+        self.model = tf.saved_model.load(os.path.join(self.cacheDir, "checkpoints", self.modelName, "saved_model"))
+
+        print("Model " + self.modelName + " loaded successfully...")
+
+
+    def createBoundingBox(self, image, threshold=0.5):
+        inputTensor = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
+        inputTensor = tf.convert_to_tensor(inputTensor, dtype=tf.uint8)
+        inputTensor = inputTensor[tf.newaxis,...]
+
+        detections = self.model(inputTensor)
+
+        bboxes = detections['detection_boxes'][0].numpy()
+        classIndexes = detections['detection_classes'][0].numpy().astype(np.int32)
+        classScores = detections['detection_scores'][0].numpy()
+
+        imH, imW, imC = image.shape
+
+        bboxIdx = tf.image.non_max_suppression(bboxes, classScores,
+                                               max_output_size=50, iou_threshold=0.5, score_threshold=0.5)
+
+        print(bboxIdx)
+        if len(bboxIdx) !=0:
+            for i in bboxIdx:
+                bbox = tuple(bboxes[i].tolist())
+                classConfidence = round(100*classScores[i])
+                classIndex = classIndexes[i]
+
+                classLabelText = self.classesList[classIndex]
+                classColor =  self.colorList[classIndex]
+
+                displayText = '{}: {}%'.format(classLabelText, classConfidence)
+                ymin, xmin, ymax, xmax = bbox
+
+                xmin, xmax, ymin, ymax = (xmin * imW, xmax * imW, ymin * imH, ymax * imH)
+                xmin, xmax, ymin, ymax = int(xmin), int(xmax), int(ymin), int(ymax)
+
+                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color=classColor, thickness=1)
+                cv2.putText(image, displayText, (xmin, ymin - 10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
+
+        return image
+
+    def predictImage(self, imagePath, threshold=0.5):
+        image = cv2.imread(imagePath)
+
+        bboxImage = self.createBoundingBox(image, threshold)
+
+        cv2.imwrite(self.modelName + ".jpg", bboxImage)
+        cv2.imshow("Result", bboxImage)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
